893ca3718fb809d1b1b53ce26f1dad1bd9408f5e
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-06-01 19:01:48 +0600

    setup doneZ

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.port || 5000
+require('dotenv').config()
 
-//Hotel-Management
-//AS3dUAOAMrXrOhOJ
 
 //middleware
 app.use(cors())
@@ -12,7 +11,7 @@ app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.mfnurby.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mfnurby.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,7 +31,7 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
