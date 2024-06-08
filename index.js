a5fe8afd364464bc7222c76f4a82b2455e1b314d
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-06-08 12:43:57 +0600

    added the get for fetch the specifice room dat

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mfnurby.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -59,6 +59,17 @@ async function run() {
             console.log(result);
         })
 
+        app.get('/rooms/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const options = {
+               
+                // Include only the `title` and `imdb` fields in the returned document
+                projection: { price: 1,images:1,room_description:1 },
+              };
+            const result=await roomsCollection.findOne(query,options)
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
