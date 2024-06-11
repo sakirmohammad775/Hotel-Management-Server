63a592384b6b646ca4675c7ec5a4b9391c249e93
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-06-11 22:19:53 +0600

    console the final topic

--- index.js
+++ index.js
@@ -92,7 +92,6 @@ async function run() {
             const query = { _id: new ObjectId(id) }
             const result=await bookingsCollection.deleteOne(query)
             res.send(result)
-            console.log(result);
         })
 
 
